42c16baed3f0f191f7a53fae6c3e666f25994efb
Michael Roussell <55032397+Mroussell@users.noreply.github.com>
2022-05-11 15:09:35 -0400

    fix(click): hotfix click focux bug by adding click handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 import Chat from './chat'
 import Bubble from './bubble'
@@ -7,6 +7,12 @@ import useMessages from './use-messages'
 import { motion, AnimatePresence } from 'framer-motion'
 
 function App() {
+  const i1 = useRef(null);
+
+  const handleClick = () => {
+    window.document.querySelector('.bubble.input > div').focus()
+  };
+
   const [messages, addMessage] = useMessages([])
   const [newMessage, setNewMessage] = useState('')
 
@@ -25,16 +31,17 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className="App" onClick={handleClick}>
       <Chat>
         <AnimatePresence>
           {messages.map(m => (
-            <Bubble key={m} id={m}>
+            <Bubble key={m} id={m} >
               {m}
             </Bubble>
           ))}
         </AnimatePresence>
         <BubbleInput
+          ref={i1}
           value={newMessage}
           onChange={setNewMessage}
           onSubmit={handleSubmit}
